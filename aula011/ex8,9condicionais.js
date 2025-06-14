9f0bd7ec430e75bb4aba46b8e65ca87ff2ed8d0b
imortalseibert02 <Felipe.Seibert.2003@gmail.com>
2025-06-14 10:00:46 -0300

    if else

---
+++ aula011/ex8,9condicionais.js
@@ -0,0 +1,17 @@
+// CONDICIONAIS SIMPLES
+// var vel = 60
+// console.log(`a velocidade do carro é de ${vel}Km/h`)
+// if (vel >= 60) {
+//     console.log('Você ultrapassou o limite de velocidade permitida, MULTADO !')
+// }
+// console.log('Dirija sempre com cinto de segurança...')
+//-------------------------
+
+// CONDICIONAIS IF ELSE
+// var pais = 'eua'
+// console.log(`Vivo no ${pais}`)
+// if (pais == 'brasil') {
+//     console.log('Vivo na minha terra natal')
+// } else {
+//     console.log ('Vivo em uma terra estrangeira')
+// }
